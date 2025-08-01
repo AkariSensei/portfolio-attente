55161e3e6e419034c389ec9e642d7f2e6936a437
Tony Constant <74190782+AkariSensei@users.noreply.github.com>
2025-08-01 10:38:48 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
-import avatar from "./assets/img/avatar-moi.png";
 import TypingText from "./TypingText";
 
 export default function Header() {
     return (
         <div className="absolute left-10 flex items-center gap-4 top-1/3">
             <img
-                src={avatar}
+                src="/avatar-moi.png"
                 alt="Avatar"
                 className="w-48 h-48 rounded-full border-4 border-[color:var(--color-primary)] shadow-lg"
             />
