a449b2c2dc5f3e6584997dc71057f013b330c116
Tony Constant <74190782+AkariSensei@users.noreply.github.com>
2025-08-01 10:46:20 +0200

    Update Timer.tsx
    Ajustement du container de secondes

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -3,22 +3,24 @@ interface TimerProps {
 }
 
 export default function Timer({ timeLeft }: TimerProps) {
+    const labels = ["jours", "heures", "minutes", "sec."];
+    const values = [timeLeft.days, timeLeft.hours, timeLeft.minutes, timeLeft.seconds];
+
     return (
         <div className="flex flex-col items-center text-center mt-24">
             <h1 className="text-5xl mb-2">Portfolio en construction</h1>
             <p className="mb-8 text-lg opacity-80">Mon nouveau portfolio arrive bientôt</p>
             <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
-                {["jours", "heures", "minutes", "secondes"].map((label, index) => {
-                    const value = [timeLeft.days, timeLeft.hours, timeLeft.minutes, timeLeft.seconds][index];
-                    return (
-                        <div key={label} className="flex flex-col p-4 bg-[rgba(34,197,94,0.15)] rounded-box">
-                            <span className="countdown font-mono text-5xl text-primary">
-                                <span style={{ "--value": value } as React.CSSProperties}></span>
-                            </span>
-                            {label}
-                        </div>
-                    );
-                })}
+                {labels.map((label, index) => (
+                    <div
+                        key={label}
+                        className="flex flex-col p-4 bg-[rgba(34,197,94,0.15)] rounded-box items-center min-w-[80px]">
+                        <span className="countdown font-mono text-5xl text-primary flex justify-center w-full">
+                            <span style={{ "--value": values[index] } as React.CSSProperties}></span>
+                        </span>
+                        {label}
+                    </div>
+                ))}
             </div>
         </div>
     );
